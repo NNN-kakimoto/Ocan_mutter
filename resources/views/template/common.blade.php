1eedf3e8cda72cae0942a10a553b3cc6203f4687
NNN <b7080@oic.jp>
2019-02-18 20:16:49 +0900

    page title yield

--- resources/views/template/common.blade.php
+++ resources/views/template/common.blade.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Laravel</title>
+	<title>@yield('title') | OCAN MUTTER</title>
 	<link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 	<link href="/css/app.css" rel="stylesheet">
