3c253a203cbbc8ec05c65a168c089c3bc05bc81a
NNN <b7080@oic.jp>
2019-02-20 23:27:40 +0900

    footer responsive recommit

--- resources/views/template/footer.blade.php
+++ resources/views/template/footer.blade.php
@@ -1,6 +1,6 @@
 <footer>
 	<div class="footer_title">OCAN MUTTER</div>
 	<div class="footer_links">
-		<span class="footer_link_item right"><a href="https://twitter.com/kakimoto_itc" target="_blank">Twitter@kakimoto_itc</a></span><span class="symbol">|</span><span class="footer_link_item"><a href="https://nnn-kakimoto.github.io/mySite" target="_blank">Kakimotoのポートフォリオサイト</a></span>
+		<span class="footer_link_item right"><a href="https://twitter.com/kakimoto_itc" target="_blank">Twitter@kakimoto_itc</a></span><span class="symbol">|</span><span class="footer_link_item"><a href="https://nnn-kakimoto.github.io/mySite" target="_blank">About Kakimoto?</a></span>
 	</div>
 </footer>
