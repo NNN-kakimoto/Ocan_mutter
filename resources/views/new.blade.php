ea7f3527ebdc9bb31ddea7ad48a5c7c87c0a94a0
NNN <b7080@oic.jp>
2019-03-01 01:05:15 +0900

    modify new.blade
    autofocus

--- resources/views/new.blade.php
+++ resources/views/new.blade.php
@@ -5,7 +5,7 @@
 
 <div class="new">
 	<div class="new_nickname">名無し さんこんにちは!</div>
-	<textarea id="new_content" rows="10"></textarea>
+	<textarea id="new_content" rows="10" autofocus></textarea>
 	<div class="new_controll">
 		<a href="/" class="new_back_button">BACK</a>
 		<button type="button" id="send" class="sended">SEND</button>
