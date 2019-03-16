8b35e2497733b720294e7493555fa61fc373a600
NNN <b7080@oic.jp>
2019-03-17 00:28:44 +0900

    modify route mask

--- routes/web.php
+++ routes/web.php
@@ -23,12 +23,15 @@ Route::get('/', function () {
 Route::get('/mutter_demo', function(){
 	return view('new');
 });
-Route::get('/mutter', function(){
-	return view('select_user');
-});
+// Route::get('/mutter', function(){
+// 	return view('select_user');
+// });
 Route::post('/branch_select', function(Request $request){
 	return redirect("/new/".$request->nickname.'_post');
 });
+// Route::get('/aaa/', function(){
+// 	return view('new/_post');
+// });
 Route::get('/csrf_token', function(){
 	return [ 
 		'token' => csrf_token(),
